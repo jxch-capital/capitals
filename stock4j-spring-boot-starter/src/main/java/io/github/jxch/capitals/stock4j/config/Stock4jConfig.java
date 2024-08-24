0b00ab2edcad50d797c1d790e85efc4d80415e11
jxch <qianbuhan@proton.me>
2024-08-24 08:32:20 +0800

    starter支持

--- stock4j-spring-boot-starter/src/main/java/io/github/jxch/capitals/stock4j/config/Stock4jConfig.java
+++ stock4j-spring-boot-starter/src/main/java/io/github/jxch/capitals/stock4j/config/Stock4jConfig.java
@@ -4,10 +4,12 @@ import lombok.Data;
 import okhttp3.OkHttpClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Data
 @Configuration
+@ComponentScan(basePackages = "io.github.jxch.capitals.stock4j")
 public class Stock4jConfig {
     public final static String STOCK_API = "CAPITAL_STOCK4J_API";
     public final static String OK_HTTP_CLIENT = "OK_HTTP_CLIENT";
