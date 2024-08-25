18fa4789dea06d0e29404e84f1556d41ce42dc4f
jxch <qianbuhan@proton.me>
2024-08-25 13:09:09 +0800

    自动配置

---
+++ stock4j-webapp-api/src/main/java/io/github/jxch/capitals/stock4j/webapp/api/Stock4jApiConfig.java
@@ -0,0 +1,9 @@
+package io.github.jxch.capitals.stock4j.webapp.api;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class Stock4jApiConfig {
+}
