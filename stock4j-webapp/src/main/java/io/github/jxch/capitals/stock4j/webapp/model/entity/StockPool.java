034bbbf84f1764299059a351b10371e2c6311b23
jxch <qianbuhan@proton.me>
2024-12-05 22:42:45 +0800

    jpa

---
+++ stock4j-webapp/src/main/java/io/github/jxch/capitals/stock4j/webapp/model/entity/StockPool.java
@@ -0,0 +1,18 @@
+package io.github.jxch.capitals.stock4j.webapp.model.entity;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+
+@Data
+//@Builder
+//@NoArgsConstructor
+//@AllArgsConstructor
+@Accessors(chain = true)
+//@Entity
+//@Comment("股票池")
+//@Table(name = "stock_pool")
+public class StockPool {
+
+
+
+}
