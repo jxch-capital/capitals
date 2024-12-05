04a7f3dcf39f21f1e1a9d6ded6a1d823f08061c0
jxch <qianbuhan@proton.me>
2024-12-05 20:24:01 +0800

    服务注册与发现

---
+++ capitals-cloud-admin/src/main/java/io/github/jxch/capitals/cloud/admin/CapitalsCloudAdminApp.java
@@ -0,0 +1,13 @@
+package io.github.jxch.capitals.cloud.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class CapitalsCloudAdminApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalsCloudAdminApp.class, args);
+    }
+}
