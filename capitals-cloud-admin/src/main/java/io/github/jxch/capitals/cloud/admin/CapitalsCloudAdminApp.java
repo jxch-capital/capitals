cda8c99dfff0b6e526c054006631b8f1a8781d9f
jxch <qianbuhan@proton.me>
2024-12-05 21:38:44 +0800

    暂时禁用配置中心（导致注册中心无法使用，等待解决方案）

--- capitals-cloud-admin/src/main/java/io/github/jxch/capitals/cloud/admin/CapitalsCloudAdminApp.java
+++ capitals-cloud-admin/src/main/java/io/github/jxch/capitals/cloud/admin/CapitalsCloudAdminApp.java
@@ -3,10 +3,9 @@ package io.github.jxch.capitals.cloud.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableAdminServer
-@EnableConfigServer
+//@EnableConfigServer
 @SpringBootApplication
 public class CapitalsCloudAdminApp {
     public static void main(String[] args) {
