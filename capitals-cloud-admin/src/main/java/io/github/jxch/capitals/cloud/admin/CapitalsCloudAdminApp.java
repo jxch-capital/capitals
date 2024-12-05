92dbdfee3c27d95b0e98728926385db592089f66
jxch <qianbuhan@proton.me>
2024-12-05 20:46:26 +0800

    配置中心

--- capitals-cloud-admin/src/main/java/io/github/jxch/capitals/cloud/admin/CapitalsCloudAdminApp.java
+++ capitals-cloud-admin/src/main/java/io/github/jxch/capitals/cloud/admin/CapitalsCloudAdminApp.java
@@ -3,8 +3,10 @@ package io.github.jxch.capitals.cloud.admin;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableAdminServer
+@EnableConfigServer
 @SpringBootApplication
 public class CapitalsCloudAdminApp {
     public static void main(String[] args) {
