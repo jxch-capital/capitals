74218e8b3ce6c57f3b887535ded6b07c8839f9e7
jxch <qianbuhan@proton.me>
2024-12-09 15:55:18 +0800

    Gateway

---
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/CapitalsCloudGatewayApp.java
@@ -0,0 +1,11 @@
+package io.github.jxch.capitals.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalsCloudGatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalsCloudGatewayApp.class, args);
+    }
+}
