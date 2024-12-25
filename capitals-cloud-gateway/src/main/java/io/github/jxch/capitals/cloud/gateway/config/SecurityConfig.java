df6a9c81f7486b860d24a505fc3280b48d8ea87c
jxch <qianbuhan@proton.me>
2024-12-26 00:24:17 +0800

    permits

--- capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.jxch.capitals.cloud.gateway.config;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,18 +9,22 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+import java.util.List;
+
+@Data
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Value("${spring.security.permits:}")
+    private List<String> permits;
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http
-                .authorizeExchange(authorize -> authorize
-                        .pathMatchers("/public/**").permitAll()
-                        .pathMatchers("/inner/**").permitAll()
-                        .anyExchange().authenticated()
-                )
+                .authorizeExchange(authorize -> {
+                    permits.forEach(permit -> authorize.pathMatchers(permit).permitAll());
+                    authorize.anyExchange().authenticated();
+                })
                 .oauth2Login(Customizer.withDefaults())
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
