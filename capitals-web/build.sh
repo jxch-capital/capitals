c4c7d847e7365d4d639070dcceef4166441f8fc2
jxch <qianbuhan@proton.me>
2024-12-28 20:01:24 +0800

    sudo

--- capitals-web/build.sh
+++ capitals-web/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
-buildx create --use
+sudo buildx create --use
 sudo docker buildx build --platform=linux/arm64,linux/amd64 -t jxch/capitals-web:$(date +%Y%m%d%H) -t jxch/capitals-web:latest . --push
