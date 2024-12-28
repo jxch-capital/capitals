2d1ded528ffd54280903c0d3db15a78f6a7e9e3c
jxch <qianbuhan@proton.me>
2024-12-28 10:54:24 +0800

    docker 支持

--- capitals-web/src/router/index.ts
+++ capitals-web/src/router/index.ts
@@ -1,18 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router';
-
+import {createRouter, createWebHistory} from 'vue-router';
 import Home from '../view/Home.vue';
+import {BackupTableFilled, HomeOutlined} from "@vicons/material";
+import type {Component} from "vue";
+import {NIcon} from 'naive-ui'
+import {h} from 'vue'
 
+function renderIcon(icon: Component) {
+    return () => h(NIcon, null, {default: () => h(icon)})
+}
+
+const pathRoot = '/capitals';
 const routes = [
     {
-        path: '/',
-        name: 'Home',
+        path: '/home',
+        name: 'home',
+        whateverLabel: '首页',
+        whateverKey: 'home',
+        icon: renderIcon(HomeOutlined),
         component: Home,
     },
-];
+    {
+        path: '/stock-pool',
+        name: 'stock-pool',
+        whateverLabel: '股票池',
+        whateverKey: 'stock-pool',
+        icon: renderIcon(BackupTableFilled),
+        component: Home,
+    }
+].map(item => {
+    return {
+        ...item,
+        path: pathRoot + item.path
+    };
+});
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
 
+export const menu = routes.filter(item => item.hasOwnProperty("whateverLabel"));
 export default router;
+
+router.afterEach((to) => {
+    document.title = (to.meta as { title?: string }).title || 'Capitals';
+});
+
