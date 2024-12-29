a78034427aec71492dde2221aa2110c57961f3bb
jxch <qianbuhan@proton.me>
2024-12-29 18:08:05 +0800

    认证逻辑

--- capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
@@ -4,11 +4,15 @@ import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.http.ResponseCookie;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.util.List;
 
 @Data
@@ -25,10 +29,28 @@ public class SecurityConfig {
                     permits.forEach(permit -> authorize.pathMatchers(permit).permitAll());
                     authorize.anyExchange().authenticated();
                 })
-                .oauth2Login(Customizer.withDefaults())
+                .oauth2Login(oauth2LoginSpec -> oauth2LoginSpec.authenticationSuccessHandler(authenticationSuccessHandler()))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable);
 
         return http.build();
+    }
+
+    @Bean
+    public ServerAuthenticationSuccessHandler authenticationSuccessHandler() {
+        return (webFilterExchange, authentication) -> {
+            OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
+            String username = oauthToken.getPrincipal().getName();
+
+            ResponseCookie cookie = ResponseCookie.from("username", username)
+                    .httpOnly(true)
+                    .secure(true)
+                    .path("/")
+                    .maxAge(Duration.ofHours(1))
+                    .build();
+
+            webFilterExchange.getExchange().getResponse().addCookie(cookie);
+            return Mono.empty();
+        };
     }
 
 }
