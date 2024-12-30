9e49754c9f197864ab5511122fe652d336d558a4
jxch <qianbuhan@proton.me>
2024-12-30 13:32:07 +0800

    自动装配

---
+++ capitals-cloud-common/src/main/java/io/github/jxch/capitals/cloud/common/CapitalsCLoudCommonAutoConfig.java
@@ -0,0 +1,14 @@
+package io.github.jxch.capitals.cloud.common;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@RefreshScope
+@Configuration
+@ComponentScan
+public class CapitalsCLoudCommonAutoConfig {
+
+
+
+}
