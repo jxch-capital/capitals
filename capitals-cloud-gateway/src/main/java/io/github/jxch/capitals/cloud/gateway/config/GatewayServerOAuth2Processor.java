568b93bd93792b4f767f59aa08315111513cec5d
jxch <qianbuhan@proton.me>
2024-12-31 11:24:08 +0800

    支持动态重定向

---
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/GatewayServerOAuth2Processor.java
@@ -0,0 +1,83 @@
+package io.github.jxch.capitals.cloud.gateway.config;
+
+import io.github.jxch.capitals.cloud.common.model.UserCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.server.DefaultServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.core.endpoint.OAuth2AuthorizationRequest;
+import org.springframework.security.web.server.WebFilterExchange;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class GatewayServerOAuth2Processor implements ServerOAuth2AuthorizationRequestResolver, ServerAuthenticationSuccessHandler {
+    private final ServerOAuth2AuthorizationRequestResolver defaultResolver;
+    private final SecurityConfig securityConfig;
+
+    public GatewayServerOAuth2Processor(ReactiveClientRegistrationRepository clientRegistrationRepository, SecurityConfig securityConfig) {
+        this.defaultResolver = new DefaultServerOAuth2AuthorizationRequestResolver(clientRegistrationRepository);
+        this.securityConfig = securityConfig;
+    }
+
+    @Override
+    public Mono<OAuth2AuthorizationRequest> resolve(ServerWebExchange exchange) {
+        return defaultResolver.resolve(exchange).map(authorizationRequest -> resolve(authorizationRequest, exchange));
+    }
+
+    @Override
+    public Mono<OAuth2AuthorizationRequest> resolve(ServerWebExchange exchange, String clientRegistrationId) {
+        return defaultResolver.resolve(exchange, clientRegistrationId).map(authorizationRequest -> resolve(authorizationRequest, exchange));
+    }
+
+    private OAuth2AuthorizationRequest resolve(OAuth2AuthorizationRequest authorizationRequest, ServerWebExchange exchange) {
+        String redirectTo = exchange.getRequest().getQueryParams().getFirst(securityConfig.getRedirectUrlParam());
+        if (StringUtils.hasText(redirectTo)) {
+            Map<String, Object> additionalParameters = new HashMap<>(authorizationRequest.getAdditionalParameters());
+            additionalParameters.put(securityConfig.getRedirectUrlParam(), redirectTo);
+            return OAuth2AuthorizationRequest.from(authorizationRequest)
+                    .additionalParameters(additionalParameters)
+                    .build();
+        }
+        return authorizationRequest;
+    }
+
+    @Override
+    public Mono<Void> onAuthenticationSuccess(WebFilterExchange webFilterExchange, Authentication authentication) {
+        UserCookie userCookie = UserCookie.parseObject(authentication.getPrincipal());
+        webFilterExchange.getExchange().getResponse().addCookie(ResponseCookie.from(UserCookie.class.getSimpleName(), userCookie.toUrlJson())
+                .httpOnly(true).secure(true).path(securityConfig.getCookiePath()).maxAge(Duration.ofHours(securityConfig.getCookieMaxAge())).build());
+        return Mono.fromRunnable(() -> {
+            ServerWebExchange exchange = webFilterExchange.getExchange();
+
+            String state = exchange.getRequest().getQueryParams().getFirst(securityConfig.getRedirectUrlParam());
+            String redirectUrl = (state != null && isValidRedirectUri(state)) ? state :
+                    UriComponentsBuilder.fromPath(securityConfig.getDefaultLoginRedirectUrl()).build().toUriString();
+
+            exchange.getResponse().setStatusCode(HttpStatus.FOUND);  // 302 重定向
+            exchange.getResponse().getHeaders().setLocation(URI.create(redirectUrl));
+        });
+    }
+
+    private boolean isValidRedirectUri(String uri) {
+        try {
+            URI parsedUri = new URI(uri);
+            String host = parsedUri.getHost();
+            return host != null && securityConfig.getAllowRedirectHost().stream().anyMatch(host::endsWith);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+}
--- capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/config/SecurityConfig.java
@@ -1,24 +1,19 @@
 package io.github.jxch.capitals.cloud.gateway.config;
 
-import io.github.jxch.capitals.cloud.common.model.UserCookie;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.util.UriComponentsBuilder;
-import reactor.core.publisher.Mono;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-import java.net.URI;
-import java.time.Duration;
+import java.util.Collections;
 import java.util.List;
 
 @Data
@@ -34,52 +29,37 @@ public class SecurityConfig {
     private String cookiePath;
     @Value("${gateway.login.default-redirect-url:/capitals/}")
     private String defaultLoginRedirectUrl;
-    @Value("${gateway.login.redirect-url-param:state}")
+    @Value("${gateway.login.redirect-url-param:redirect-to}")
     private String redirectUrlParam;
     @Value("${gateway.login.allow-redirect-host:localhost,127.0.0.1}")
     private List<String> allowRedirectHost;
 
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, GatewayServerOAuth2Processor requestResolver) {
         http
+                .cors(corsSpec -> corsSpec.configurationSource(configurationSource()))
                 .csrf(csrfSpec -> csrfSpec.requireCsrfProtectionMatcher((exchange) -> ServerWebExchangeMatcher.MatchResult.notMatch()))
                 .authorizeExchange(authorize -> {
                     permits.forEach(permit -> authorize.pathMatchers(permit).permitAll());
                     authorize.anyExchange().authenticated();
                 })
-                .oauth2Login(oauth2LoginSpec -> oauth2LoginSpec
-                        .authenticationSuccessHandler(authenticationSuccessHandler()));
+                .oauth2Login(oauth2LoginSpec -> oauth2LoginSpec.authorizationRequestResolver(requestResolver)
+                        .authenticationSuccessHandler(requestResolver));
 
         return http.build();
     }
 
     @Bean
-    public ServerAuthenticationSuccessHandler authenticationSuccessHandler() {
-        return (webFilterExchange, authentication) -> {
-            UserCookie userCookie = UserCookie.parseObject(authentication.getPrincipal());
-            webFilterExchange.getExchange().getResponse().addCookie(ResponseCookie.from(UserCookie.class.getSimpleName(), userCookie.toUrlJson())
-                    .httpOnly(true).secure(true).path(cookiePath).maxAge(Duration.ofHours(cookieMaxAge)).build());
-            return Mono.fromRunnable(() -> {
-                ServerWebExchange exchange = webFilterExchange.getExchange();
+    public CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+        configuration.addAllowedHeader(CorsConfiguration.ALL);
+        configuration.addAllowedMethod(CorsConfiguration.ALL);
+        configuration.setAllowCredentials(true);
 
-                String state = exchange.getRequest().getQueryParams().getFirst(redirectUrlParam);
-                String redirectUrl = (state != null && isValidRedirectUri(state)) ? state :
-                        UriComponentsBuilder.fromPath(defaultLoginRedirectUrl).build().toUriString();
-
-                exchange.getResponse().setStatusCode(HttpStatus.FOUND);  // 302 重定向
-                exchange.getResponse().getHeaders().setLocation(URI.create(redirectUrl));
-            });
-        };
-    }
-
-    private boolean isValidRedirectUri(String uri) {
-        try {
-            URI parsedUri = new URI(uri);
-            String host = parsedUri.getHost();
-            return host != null && allowRedirectHost.stream().anyMatch(host::endsWith);
-        } catch (Exception e) {
-            return false;
-        }
+        org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
