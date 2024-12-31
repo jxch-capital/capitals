5568bc06fda3a760716be5a18fe5730c61318293
jxch <qianbuhan@proton.me>
2024-12-31 09:34:15 +0800

    移除登录接口

--- capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/controller/GatewayUserController.java
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/controller/GatewayUserController.java
@@ -1,31 +1,19 @@
 package io.github.jxch.capitals.cloud.gateway.controller;
 
 import io.github.jxch.capitals.cloud.common.model.UserCookie;
-import io.github.jxch.capitals.cloud.gateway.config.SecurityConfig;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-import java.net.URI;
 import java.util.Objects;
 
 @RestController
 @RequestMapping("/")
 @RequiredArgsConstructor
 public class GatewayUserController {
-    private final SecurityConfig securityConfig;
-
-    @RequestMapping("/user/login")
-    public Mono<Void> login(ServerHttpResponse response) {
-        response.setStatusCode(HttpStatus.FOUND);
-        response.getHeaders().setLocation(URI.create(securityConfig.getLoginRedirectUrl()));
-        return Mono.empty();
-    }
 
     @RequestMapping("/public/userCookie")
     public Mono<UserCookie> getUserCookie(Authentication authentication, ServerHttpRequest request) {
