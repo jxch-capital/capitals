9d47b01db0a78952508639f766874ed3df67e257
jxch <qianbuhan@proton.me>
2025-01-02 16:30:12 +0800

    拿session

--- capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/controller/PublicController.java
+++ capitals-cloud-gateway/src/main/java/io/github/jxch/capitals/cloud/gateway/controller/PublicController.java
@@ -5,14 +5,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/public")
 public class PublicController {
 
     @RequestMapping("/session")
-    public Mono<String> session(WebSession session) {
+    public Mono<Map<String, Object>> session(WebSession session) {
         session.getAttributes().put("session", session.getId());
-        return Mono.just(session.getId());
+        return Mono.just(session.getAttributes());
     }
 
 }
